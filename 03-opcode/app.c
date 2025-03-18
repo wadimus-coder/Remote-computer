a3095d25243f30ac802ac2879a7924d1f15d1b67
Wadim <grusa1360@gmail.com>
2025-03-18 15:46:12 +0200

    03-opcode part2

--- 03-opcode/app.c
+++ 03-opcode/app.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
 
+struct s_cmd_pack
+{
+    short opcode;
+    short len;
+    char data[1000];
+};
+
+void print_pack(struct s_cmd_pack *obj_cmd_pack)
+{
+    printf("opcode : %d\n", obj_cmd_pack->opcode);
+    printf("len : %d\n", obj_cmd_pack->len);
+    printf("data : %s\n", obj_cmd_pack->data);
+}
+
 void exec_data(char *cmd, char *data)
 {
 
@@ -20,12 +34,26 @@ void exec_data(char *cmd, char *data)
 
 int main()
 {
-    // char cmd[100] = "type app.c";
+    /*
     char data[5000];
-    // exec_data("folder1", data);
-    chdir("folder1"); // change DIRectory
+    char cmd[100] = "cd folder1";
+
+    char new_path[100];
+    // memset(new_path, 0, sizeof(new_path));
+    new_path[0] = 0;
+    sscanf(cmd, "cd %s", new_path);
+
+    chdir(new_path); // change DIRectory
     exec_data("dir", data);
 
     printf("%s\n", data);
     return 0;
+    */
+    struct s_cmd_pack pack = {0, 3, "dir"};
+    print_pack(&pack);
 }
+
+// opcode =0 : обычная команда
+// opcode =1 : смена директории
+// opcode =2 : скачать файл для редактирования
+// opcode =3 : отправить отред.копию файла на сервер
